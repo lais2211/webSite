cdba716d70a17e266da86f18ac44482c1bc33087
Joao Gomes <joao.gomes@stuo.app>
2024-09-17 12:42:00 -0300

    feat: retirada de botao download

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,15 +4,7 @@ import TextEffect from './TextEffect';
 import Image from 'next/image';
 import ArrowDownTrayIcon from '@heroicons/react/16/solid/ArrowDownTrayIcon';
 const Hero = () => {
-    const onButtonClick = () => {
-        const pdfUrl = "/pdf/cvLais.pdf";
-        const link = document.createElement("a");
-        link.href = pdfUrl;
-        link.download = "curriculo Lais Sousa.pdf"; 
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+   
     return (
         <div className='h-[100vh] bg-[url("/images/banner.jpg")] bg-cover bg-center'>
             <Particle />
@@ -22,12 +14,6 @@ const Hero = () => {
                         Olá, eu sou a <span className='text-yellow-400'>Laís!</span>
                     </h1>
                     <TextEffect />
-                    <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex-row items-center sm:space-x-6'>
-                        <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[irem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2 ' onClick={onButtonClick}>
-                            <p>Download Cv</p>
-                            <ArrowDownTrayIcon className='w-[1.6rem] h-[2.7rem] text-black' />
-                        </button>
-                    </div>
                 </div>
                 <div className='lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[225px] h-[225px] relative items-center rounded-full'>
                     <Image src="/images/u1.jpg" alt="user" layout='fill' className='object-cover rounded-full' />
